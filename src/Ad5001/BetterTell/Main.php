4eb5b7881355a957dea2ff554475d2f32c304cd8
Ad5001 <ad5001yt@gmail.com>
2016-01-06 17:51:59 +0100

    Update Main.php

--- src/Ad5001/BetterTell/Main.php
+++ src/Ad5001/BetterTell/Main.php
@@ -2,10 +2,9 @@
 namespace Ad5001\BetterTell;
 
 /*
-*
-*
-*
-*
+                                         BetterTell Plugin by Ad5001 ! 
+                                       This plugin is work in progress! 
+                          Feel free to make issues or/and help me correcting bugs :)
 */
 
 use pocketmine\command\CommandSender;
@@ -13,18 +12,18 @@ use pocketmine\command\Command;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
-  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-    switch($cmd->getName()){ // Nom de la commande
-      case "tellraw":
-	      $sender->sendMessage["§a§l[Tellraw]§r§a Message has been display if you had tipe EXACTLY the player name!"];
-		  $telled = $args[0];
-		  $message = $args[1]->getMessage();
-		  $telled->getPlayerExact->sendMessage("$message");
-        return true; // return true always unless you want the usage message be shown: return false
-		break;
-	  default:
-	      $sender->sendMessage("§4Unknown command " . $cmd . ". Try /help for the list of commands");
-      }
+          public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+                 switch($cmd->getName()){ // Command name
+                         case "tellraw":
+                                 $sender->sendMessage["§a§l[Tellraw]§r§a Message has been display if you had tipe EXACTLY the player name!"];
+                                 $telled = $args[0];
+                                 $message = $args[1]->getMessage();
+                                 $telled->getPlayerExact->sendMessage("$message");
+                                 return true;
+                                 break;
+                         default:
+                                 $sender->sendMessage("§4Unknown command " . $cmd . ". Try /help for the list of commands");
+         }
   public function onDisable() {
     $this->getLogger()->info("Test plugin has been disable!");
    }
